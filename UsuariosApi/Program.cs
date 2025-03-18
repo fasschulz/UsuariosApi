fe5b68180eb7f3b4cadd8a2481b0a39d5c3ba4fa
Fernando Schulz <fasschulz@gmail.com>
2025-03-18 14:50:28 -0300

    Criação das tabelas do Identity no banco

--- UsuariosApi/Program.cs
+++ UsuariosApi/Program.cs
@@ -1,6 +1,22 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using UsuariosApi.Data;
+using UsuariosApi.Models;
 
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("UsuarioConnection");
 // Add services to the container.
+
+builder.Services.AddDbContext<UsuarioDbContext>
+    (opts =>
+    {
+        opts.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+    });
+
+builder.Services
+    .AddIdentity<Usuario, IdentityRole>()
+    .AddEntityFrameworkStores<UsuarioDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
