1e7e0a8392bcb30543aa9b13763a419cf66ec4a1
Fernando Schulz <fasschulz@gmail.com>
2025-03-18 15:23:08 -0300

    Cadastrando usuario no banco

--- UsuariosApi/Controllers/UsuarioController.cs
+++ UsuariosApi/Controllers/UsuarioController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using UsuariosApi.Data.DTOs;
+using UsuariosApi.Models;
 
 namespace UsuariosApi.Controllers;
 
@@ -7,9 +10,25 @@ namespace UsuariosApi.Controllers;
 [Route("[Controller]")]
 public class UsuarioController : ControllerBase
 {
-    [HttpPost]
-    public IActionResult CadastraUsuario(CreateUsuarioDto dto)
+    private IMapper _mapper;
+    private UserManager<Usuario> _userManager;
+
+    public UsuarioController(IMapper mapper, UserManager<Usuario> userManager)
     {
-        throw new NotImplementedException();
+        _mapper = mapper;
+        _userManager = userManager;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CadastraUsuario(CreateUsuarioDto dto)
+    {
+        Usuario usuario = _mapper.Map<Usuario>(dto);
+
+        var result = await _userManager.CreateAsync(usuario, dto.Password);
+
+        if (result.Succeeded)
+            return Ok("Usuário Cadastrado");
+        else
+            throw new ApplicationException(result.Errors.FirstOrDefault().Description);
     }
 }
